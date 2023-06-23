b7488ebdbd72374874b2a931afef681e4c5cf56e
namira121 <127211008+namira121@users.noreply.github.com>
2023-06-23 14:55:31 +0700

    mengecek jika queue penuh atau tidak

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -15,5 +15,12 @@ public:
 		int num;
 		cout << "Enter a number : "; 
 		cin >> num;
+		cout << endl; 
+
+		// cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overfrlow\n";
+			return;
+		}
 	}
 };
