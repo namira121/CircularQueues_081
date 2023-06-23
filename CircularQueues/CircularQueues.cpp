3f5c2a28e1248480d9808c234ff4ea9d2f8b1480
namira121 <127211008+namira121@users.noreply.github.com>
2023-06-23 15:11:24 +0700

    jika tidak maka pindahkan front ke index selanjutnya

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -55,8 +55,8 @@ public:
 			// jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
 			if (FRONT == max - 1)
 				FRONT = 0;
-			else 
-				FRONT =FRONT+
+			else
+				FRONT = FRONT + 1;
 		}
 	}
 };
